6901e0d545ef4026cb84ea2dba40c81bb70a6f47
kkamara <kamaracomputers@gmail.com>
2024-10-16 15:52:20 +0100

    Fix namespaces

--- app/Models/V1/Book.php
+++ app/Models/V1/Book.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\V1;
 
 use App\Enums\V1\BookApproved;
 use App\Enums\V1\BookEdition;
--- app/Models/V1/Review.php
+++ app/Models/V1/Review.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\V1;
 
 use App\Enums\V1\ReviewApproved;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
