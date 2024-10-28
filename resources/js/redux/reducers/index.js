f24a4438d109a098b443c98409d4f1e7fac9f4a0
kkamara <kamaracomputers@gmail.com>
2024-10-28 16:11:26 +0000

    Remove Users Redux resource

--- resources/js/redux/reducers/index.js
+++ resources/js/redux/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers, } from 'redux'
 import authReducer from './authReducer'
-import usersReducer from './usersReducer'
 import homeReducer from './homeReducer'
 import bookReducer from './bookReducer'
 import reviewsReducer from './reviewsReducer'
 
 export default combineReducers({
   auth: authReducer,
-  users: usersReducer,
   home: homeReducer,
   book: bookReducer,
   reviews: reviewsReducer,
