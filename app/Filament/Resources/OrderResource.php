e7b31e5c46eb118642942d64f72fd0a90888b3f1
kkamara <kamaracomputers@gmail.com>
2024-11-05 01:29:17 +0000

    Add Orders resource to admin area

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,13 +2,18 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\V1\OrderStatus;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\V1\Order;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\TextInputColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +28,15 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Select::make("user")
+                    ->relationship("user", "name")
+                    ->preload()
+                    ->searchable(),
+                TextInput::make("cost"),
+                TextInput::make("delivery_cost"),
+                Select::make("status")
+                    ->options(OrderStatus::class)
+                    ->selectablePlaceholder(false),
             ]);
     }
 
@@ -31,7 +44,12 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextInputColumn::make("user.name"),
+                TextInputColumn::make("cost"),
+                TextInputColumn::make("delivery_cost"),
+                SelectColumn::make("status")
+                    ->options(OrderStatus::class)
+                    ->selectablePlaceholder(false),
             ])
             ->filters([
                 //
